d1353da436da864a9ce35b06479860de8fe4e848
fanuelnalem <fanuelnalem@outlook.com>
2020-05-08 12:22:47 -0700

    heroku deploy

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes");
+const apiRoutes = require("./routes/apiroutes");
+const htmlRoutes = require("./routes/htmlroutes");
 
 // Initialize the app and create a port
 const app = express();
