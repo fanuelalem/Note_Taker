107c60dc209aded29d1b3eeb359a559bd7f640aa
fanuelnalem <fanuelnalem@outlook.com>
2020-05-25 09:13:30 -0700

    we

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
+const cors = require('cors')
 
 // Initialize the app and create a port
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001
 
 // Set up body parsing, static, and route middleware
+app.use(cors())
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
